a47af3f46e41b23c9d33867ca070bcaa8f93d07a
handshakinglemma <emcdonal@ualberta.ca>
2016-02-11 15:44:48 -0700

    Lab 6 stuff

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
@@ -2,88 +2,59 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.ViewAsserts;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.widget.TextView;
 
-/**
- * Created by sajediba on 2/8/16.
- */
-public class IntentReaderActivityTest extends ActivityInstrumentationTestCase2{
+public class IntentReaderActivity extends Activity {
 
-    public IntentReaderActivityTest() {
-        super(IntentReaderActivity.class);
+    public static final String TEXT_TO_TRANSFORM_KEY = "TEXT";
+    public static final String MODE_OF_TRANSFORM_KEY = "TRANSFORM";
+
+    public static final int NORMAL = 1;
+    public static final int REVERSE = 2;
+    public static final int DOUBLE = 3;
+
+    private String text;
+    private int mode;
+
+    public String getText() {
+        return text;
     }
 
-    //
-    //
-    public void testSendText() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 2");
-        setActivityIntent(intent);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_reader);
 
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        assertEquals("IntentReaderActivity should get the text from intent!", "Test message 2", intentReaderActivity.getText());
+        //
+        //
+        Intent intent = getIntent();
+        mode = intent.getIntExtra(MODE_OF_TRANSFORM_KEY, NORMAL);
+        if (intent.getStringExtra(TEXT_TO_TRANSFORM_KEY) != null) {
+            text = transformText(intent.getStringExtra(TEXT_TO_TRANSFORM_KEY));
+        } else {
+            text = "default value";
+        }
+        ((TextView)findViewById(R.id.intentText)).setText(text);
+        //
+        //
     }
 
-    public void testDisplayText() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 3");
-        setActivityIntent(intent);
-
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        TextView textView = (TextView) intentReaderActivity.findViewById(R.id.intentText);
-        assertEquals("Text should be displayed in the TextView!",
-                textView.getText().toString(), "Test message 3");
+    public String transformText(String str) {
+        switch (mode) {
+            case REVERSE:
+                char[] str2 =  str.toCharArray();
+                for (int i = 0; i < str2.length / 2; i++) {
+                    char tmp = str2[i];
+                    str2[i] = str2[str2.length - i -1];
+                    str2[str2.length - i - 1] = tmp;
+                }//for.
+                return new String(str2);
+            case DOUBLE:
+                return str + str;
+            default:
+                return str;
+        }
     }
-
-    public void testDoubleText() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 4");
-        intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.DOUBLE);
-        setActivityIntent(intent);
-
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        assertEquals("The text should be repeated!", "Test message 4Test message 4",
-                intentReaderActivity.getText());
-    }
-
-    //
-    //
-
-    // Tests written for Lab 6 assignment.
-
-    public void testReverseText() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 5");
-        intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.REVERSE);
-        setActivityIntent(intent);
-
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        assertEquals("The text should be reversed!", "5 egassem tseT",
-                intentReaderActivity.getText());
-    }
-
-    public void testDefaultMessage() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "default message");
-        setActivityIntent(intent);
-
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        assertEquals("The text should be reversed!", "default message",
-                intentReaderActivity.getText());
-    }
-
-    public void testTextViewVisible() {
-        Intent intent = new Intent();
-        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 6");
-        setActivityIntent(intent);
-
-        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
-        TextView textView = (TextView) intentReaderActivity.findViewById(R.id.intentText);
-        ViewAsserts.assertOnScreen(textView, intentReaderActivity.getWindow().getDecorView());
-    }
-//-------------------------------------------------------------------------------
-
-//-------------------------------------------------------------------------------
 }
